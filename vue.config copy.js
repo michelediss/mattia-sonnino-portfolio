7add3b5979ee2148a4782a51f0927aa32d568a94
michelediss <mic.paolino@gmail.com>
2025-03-24 04:06:00 +0100

    pages update vue.config

---
+++ vue.config copy.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/mattia-sonnino-portfolio/' : '/',
+  
+
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
+      })
+    ]
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        },
+      },
+    },
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,29 @@
+// vue.config.js
 const webpack = require('webpack');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/tailwind-konf/' : '/',
-  
+  // Configura il publicPath per GitHub Pages
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        // Assicurati che compilerOptions esista
+        options.compilerOptions = options.compilerOptions || {};
+        return options;
+      });
+  },
+
+  // Aggiungi configurazioni Webpack aggiuntive
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        __VUE_OPTIONS_API__: JSON.stringify(true),
-        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
-      })
-    ]
-  },
-  css: {
-    loaderOptions: {
-      postcss: {
-        postcssOptions: {
-          plugins: [
-            require('tailwindcss'),
-            require('autoprefixer'),
-          ],
-        },
-      },
-    },
+        __VUE_OPTIONS_API__: true, // Assicura che l'API delle opzioni di Vue sia disponibile
+        __VUE_PROD_DEVTOOLS__: false, // Disabilita gli strumenti di sviluppo Vue in produzione
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // Disabilita il controllo del mismatch di hydration
+      }),
+    ],
   },
 };
